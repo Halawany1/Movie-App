521856bac7b86328d6f77c754ef0c846b8115089
Halawany1 <mdnjdhdhd44@gmail.com>
2023-11-28 21:22:52 +0200

    Revert "mo"
    This reverts commit c3b985f331af134a1ebdc7c27431eb6e6c0db02d.

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: true,
-      title: 'Movie App dfsfsdfsdfqwerwrf halawayu',
+      title: 'Movie App gsdfgdfq',
       home:  MainMoviesScreen()
     );
   }
