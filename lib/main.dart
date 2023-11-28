ef05b60f7d351d7f8031d9deeb99d12539af25e0
Halawany1 <mdnjdhdhd44@gmail.com>
2023-11-28 21:42:28 +0200

    q

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'Movie App gsdfgdfq',
+      debugShowCheckedModeBanner: false,
+      title: 'Movie App sdfds',
       home:  MainMoviesScreen()
     );
   }
