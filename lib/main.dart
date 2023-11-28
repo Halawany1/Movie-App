e7ac014762553deede64e8cd3ba03de0e86845c5
Halawany1 <mdnjdhdhd44@gmail.com>
2023-11-28 21:09:22 +0200

    second change

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: true,
-      title: 'Movie App',
+      title: 'Movie App sdfs',
       home:  MainMoviesScreen()
     );
   }
