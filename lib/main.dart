d6c0ada23f80034ec33bcefe705452f4b8413435
Halawany1 <mdnjdhdhd44@gmail.com>
2023-11-28 22:44:31 +0200

    qq

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    //mp app
     return const MaterialApp(
       debugShowCheckedModeBanner: true,
       title: 'Movie App',
